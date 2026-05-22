8cc74ec98f4ae77645f1127ecc3dc2f975d11860
NQT4192 <141619421+thangnqph44861@users.noreply.github.com>
2026-05-22 19:48:35 +0700

    dns

--- config/ConnectDB.js
+++ config/ConnectDB.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
 
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 mongoose
   .connect(process.env.URL_MONGODB + "FMobileStore")
   .then(() => console.log("connect successfully ✅ !!!"))
